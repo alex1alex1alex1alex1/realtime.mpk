615597b242405c7e56df40b2bf11349d6441a1c3
alex1alex1alex1alex1 <alex1alex1alex1alex1@yandex.ru>
2020-11-24 01:30:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Show real time from your system in the game
+How to:
+1. Put **realtime.mpk** in **Data/Mods/**
+2. Add ```<mod>realtime_0.0.2</mod>``` to **ModsConfig.xml** in Data before **</mods>**
---
+++ UI/Controls/Game/HUD/HUDGameTimeIndicator.xaml.cs
@@ -0,0 +1,120 @@
+﻿namespace AtomicTorch.CBND.CoreMod.UI.Controls.Game.HUD
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Media;
+    using AtomicTorch.CBND.CoreMod;
+    using AtomicTorch.CBND.CoreMod.Systems.PvE;
+    using AtomicTorch.CBND.CoreMod.Systems.TimeOfDaySystem;
+    using AtomicTorch.CBND.CoreMod.ClientComponents.Misc;
+    using AtomicTorch.GameEngine.Common.Client.MonoGame.UI;
+
+    
+
+    public partial class HUDGameTimeIndicator : BaseUserControl
+    {
+        public static readonly DependencyProperty CurrentTimeTextProperty =
+            DependencyProperty.Register(nameof(CurrentTimeText),
+                                        typeof(string),
+                                        typeof(HUDGameTimeIndicator),
+                                        new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty TextBrushProperty =
+            DependencyProperty.Register("TextBrush",
+                                        typeof(Brush),
+                                        typeof(HUDGameTimeIndicator),
+                                        new PropertyMetadata(default(Brush)));
+
+        public static readonly DependencyProperty RealTimeTextProperty =
+            DependencyProperty.Register(nameof(RealTimeText),
+                                        typeof(string),
+                                        typeof(HUDGameTimeIndicator),
+                                        new PropertyMetadata(default(string)));
+
+        // To easily determine the PvP/PvE server on a Twitch stream or let's play video,
+        // we give a bit different tint to the time indicator.
+        private static readonly Brush BrushPvE = new SolidColorBrush(Color.FromArgb(0xAA, 0xBB, 0xCC, 0xFF));
+
+        private static readonly Brush BrushPvP = new SolidColorBrush(Color.FromArgb(0xAA, 0xFF, 0xBB, 0xBB));
+
+        private readonly StringBuilder stringBuilder = new StringBuilder();
+
+        private int lastHours = int.MaxValue;
+
+        private int lastMinutes = int.MaxValue;
+
+        String realTimeNow = DateTime.Now.ToString("HH:mm:ss");
+
+        public string RealTimeText
+        {
+            get => (string)this.GetValue(RealTimeTextProperty);
+            set => this.SetValue(RealTimeTextProperty, value);
+        }
+
+        public string CurrentTimeText
+        {
+            get => (string)this.GetValue(CurrentTimeTextProperty);
+            set => this.SetValue(CurrentTimeTextProperty, value);
+        }
+
+        public Brush TextBrush
+        {
+            get => (Brush)this.GetValue(TextBrushProperty);
+            set => this.SetValue(TextBrushProperty, value);
+        }
+
+        protected override void InitControl()
+        {
+        }
+
+        protected override void OnLoaded()
+        {
+            ClientUpdateHelper.UpdateCallback += this.Update;
+            PveSystem.ClientIsPvEChanged += this.RefreshTextBrush;
+            this.RefreshTextBrush();
+            this.RealTimeText = realTimeNow;
+        }
+
+        protected override void OnUnloaded()
+        {
+            ClientUpdateHelper.UpdateCallback -= this.Update;
+            PveSystem.ClientIsPvEChanged -= this.RefreshTextBrush;
+        }
+
+        private void RefreshTextBrush()
+        {
+            this.TextBrush = PveSystem.SharedIsPve(clientLogErrorIfDataIsNotYetAvailable: false)
+                                 ? BrushPvE
+                                 : BrushPvP;
+        }
+
+        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
+        private void Update()
+        {
+            var time = TimeOfDaySystem.CurrentTimeOfDayHours;
+            var hours = (int)time;
+            var minutes = (int)(60 * ((time - hours) % 1.0));
+
+            if (this.lastHours == hours
+                && this.lastMinutes == minutes)
+            {
+                return;
+            }
+
+            this.lastHours = hours;
+            this.lastMinutes = minutes;
+
+            this.stringBuilder
+                .Append(hours.ToString("00"))
+                .Append(":")
+                .Append(minutes.ToString("00"));
+
+            this.CurrentTimeText = this.stringBuilder.ToString();
+            this.stringBuilder.Clear();
+
+            this.RealTimeText = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
